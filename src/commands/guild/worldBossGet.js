49a39cf0fe9a830e868d2e52547b1433b913a40b
Yaki <fedemontanari@outlook.com>
2024-01-20 20:27:07 -0300

    Added green drakes to unprivileged command

--- src/commands/guild/worldBossGet.js
+++ src/commands/guild/worldBossGet.js
@@ -2,26 +2,42 @@ const { SlashCommandBuilder } = require("discord.js");
 const WorldBoss = require("../../models/worldBosses");
 const { WorldBossEmbedBuilder } = require("../../utils/worldBossEmbedBuilder");
 
+const stringOptions = new SlashCommandStringOption()
+  .setName("name")
+  .setDescription("Select the name of the boss")
+  .setRequired(true)
+  .addChoices(
+    {
+      name: "Kazzak",
+      value: "kazzak",
+    },
+    {
+      name: "Azuregos",
+      value: "azuregos",
+    },
+    {
+      name: "Emeriss",
+      value: "emeriss",
+    },
+    {
+      name: "Lethon",
+      value: "lethon",
+    },
+    {
+      name: "Taerar",
+      value: "taerar",
+    },
+    {
+      name: "Ysondre",
+      value: "ysondre",
+    }
+  );
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("showboss")
     .setDescription("Show World Boss timers")
-    .addStringOption((option) =>
-      option
-        .setName("name")
-        .setDescription("Select the name of the boss")
-        .setRequired(true)
-        .addChoices(
-          {
-            name: "Kazzak",
-            value: "kazzak",
-          },
-          {
-            name: "Azuregos",
-            value: "azuregos",
-          }
-        )
-    ),
+    .addStringOption(stringOptions),
   async execute(interaction) {
     await interaction.deferReply();
 
